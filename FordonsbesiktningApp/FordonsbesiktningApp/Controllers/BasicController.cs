53a0b4693aa5299eb87ffa80ec7e72d3949686b9
Hargaaya <hargaaya10@gmail.com>
2022-05-10 02:43:41 +0200

    Api boilerplate 🍳

---
+++ FordonsbesiktningApp/FordonsbesiktningApp/Controllers/BasicController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FordonsbesiktningApp.Controllers
+{
+    [ApiController]
+    [Route("api/basic")]
+    public class BasicController : ControllerBase
+    {
+        [HttpGet("names")]
+        public ActionResult<List<string>> Index()
+        {
+            var names = new List<string> { "Brahnen", "Jard", "Sauzie", "Erson" };
+            return names;
+        }
+    }
+}
--- FordonsbesiktningApp/FordonsbesiktningApp/Program.cs
+++ FordonsbesiktningApp/FordonsbesiktningApp/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.UseDeveloperExceptionPage();
+}
 
 app.Run();
