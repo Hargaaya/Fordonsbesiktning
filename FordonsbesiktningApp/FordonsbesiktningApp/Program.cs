7d2fab027089f9f881e15a6de7dcd9ac436ea5be
Hargaaya <hargaaya10@gmail.com>
2022-05-20 16:24:50 +0200

    Configured Cors + Minor changes

--- FordonsbesiktningApp/FordonsbesiktningApp/Program.cs
+++ FordonsbesiktningApp/FordonsbesiktningApp/Program.cs
@@ -2,22 +2,25 @@ using FordonsbesiktningApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(opt => opt.AddPolicy(name: "DevCorsPolicy", policy =>
+ {
+     policy.WithOrigins("http://localhost:3000");
+
+ }));
+
 builder.Services.AddScoped<IDataService, DataService>();
-builder.Services.AddCors(opt => opt.AddPolicy(name: "Frontend", policy =>
-{
-    policy.AllowAnyOrigin();
-    policy.AllowAnyMethod();
-    policy.AllowAnyHeader();
-}));
+
 
 var app = builder.Build();
 
 app.UseRouting();
-app.UseCors();
+app.UseCors("DevCorsPolicy");
 
 if (app.Environment.IsDevelopment())
 {
