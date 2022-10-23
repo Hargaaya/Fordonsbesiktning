65c6dc4e651ae74e7e6c55d37b6bad2d5a6fb9b8
Hargaaya <hargaaya10@gmail.com>
2022-10-23 15:35:47 +0200

    Added tailwind for next app

---
+++ FordonsbesiktningApp/fordonsbesiktning-frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3a0ca3",
+        secondary: "#560bad",
+        tertiary: "#7209b7",
+        tertiary_lighter: "#a061ca",
+      },
+    },
+  },
+  plugins: [],
+};
