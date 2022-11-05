a2fc852bf57a478df2e9a160eb75fb60042fbcb8
Hargaaya <hargaaya10@gmail.com>
2022-11-05 17:52:49 +0100

    Added typescript

--- Applications/FordonsbesiktningFrontend/pages/index.tsx
+++ Applications/FordonsbesiktningFrontend/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
@@ -10,7 +9,6 @@ export default function Home() {
         <meta name="description" content="Web app that helps you with inspecting vehicles according to TSFS 2017:54" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Sidebar />
     </div>
   );
 }
