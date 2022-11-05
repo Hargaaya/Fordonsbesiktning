f0dfdf42bd9bb3d97ac8bcc5b598e88959583545
Hargaaya <hargaaya10@gmail.com>
2022-11-05 22:46:16 +0100

    Configured tailwind for storybook

--- Applications/FordonsbesiktningFrontend/tailwind.config.js
+++ Applications/FordonsbesiktningFrontend/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./styles/*.css"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/*.css",
+    "./components/**/*.css",
+    "./components/**/*.stories.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -8,6 +14,7 @@ module.exports = {
         secondary: "#560bad",
         tertiary: "#7209b7",
         tertiary_lighter: "#a061ca",
+        gray: "rgb(129, 129, 129, 0.2)",
       },
     },
   },
