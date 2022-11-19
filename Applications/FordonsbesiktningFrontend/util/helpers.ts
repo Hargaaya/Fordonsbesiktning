8474aa15de608ac7efb2cf94c15ec06784565c34
Hargaaya <hargaaya10@gmail.com>
2022-11-20 00:52:52 +0100

    add data check helpers

--- Applications/FordonsbesiktningFrontend/util/helpers.ts
+++ Applications/FordonsbesiktningFrontend/util/helpers.ts
@@ -1,3 +1,21 @@
+import { ControlProgram } from "../util/types";
+
 export function capitalize(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function hasComponentData(data: ControlProgram): boolean {
+  if (!data.method.length && !data.control.length && data.assessment == null) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export function isEmpty(data: Array<any>): boolean {
+  return data.length == 0;
+}
+
+export function isNull(data: any): boolean {
+  return data == null;
 }
