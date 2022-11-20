3d3764dce62ef3ada646db21632f9485e5b77d87
Hargaaya <hargaaya10@gmail.com>
2022-11-20 02:08:05 +0100

    add function to convert array to readable string

--- Applications/FordonsbesiktningFrontend/util/helpers.ts
+++ Applications/FordonsbesiktningFrontend/util/helpers.ts
@@ -4,6 +4,22 @@ export function capitalize(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+export function lowercase(string: string): string {
+  return string.charAt(0).toLowerCase() + string.slice(1);
+}
+
+export function readableList(arr: Array<string>): string {
+  let result = arr[0];
+  if (arr.length > 3) result += ", ";
+  result += arr
+    .slice(1, arr.length - 2)
+    .map((item) => lowercase(item))
+    .join(", ");
+  result += " och " + lowercase(arr[arr.length - 1]);
+
+  return result;
+}
+
 export function hasComponentData(data: ControlProgram): boolean {
   if (!data.method.length && !data.control.length && data.assessment == null) {
     return false;
