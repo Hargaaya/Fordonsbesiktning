79dec8f55fc31e85f81fb6ef2255f52b21e57d8a
Hargaaya <hargaaya10@gmail.com>
2022-12-18 20:45:57 +0100

    convert svg to components

---
+++ Applications/FordonsbesiktningFrontend/assets/Hamburger.tsx
@@ -0,0 +1,7 @@
+const Hamburger = () => (
+  <svg width="19" height="15" viewBox="0 0 21 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1 1H20M1 8.5H20M1 16H20" stroke="black" stroke-width="1.5" stroke-linecap="round" />
+  </svg>
+);
+
+export default Hamburger;
