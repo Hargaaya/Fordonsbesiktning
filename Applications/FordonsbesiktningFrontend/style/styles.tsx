51b350d526cace3832e13641a03f768840b83fe7
Hargaaya <hargaaya10@gmail.com>
2022-12-18 22:25:00 +0100

    add animation to wrapper

--- Applications/FordonsbesiktningFrontend/style/styles.tsx
+++ Applications/FordonsbesiktningFrontend/style/styles.tsx
@@ -2,6 +2,18 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   margin: 10px;
+  animation: down 300ms ease-in-out;
+  transform-origin: top center;
+
+  @keyframes down {
+    0% {
+      transform: translateY(-1%);
+    }
+
+    100% {
+      transform: translateY(0);
+    }
+  }
 `;
 
 export const Header = styled.h2`
@@ -10,6 +22,7 @@ export const Header = styled.h2`
   background-clip: text;
   -webkit-text-fill-color: transparent;
   font-size: 1.2em;
+  user-select: none;
 `;
 
 export const Bolden = styled.b`
